b4cdd59ebfb07c1546ed336ea66070e018bc2565
John Cassidy <jpcassidy@outlook.com>
2023-10-30 10:24:24 -0400

    add Youtube.jsx to both classic and vite projects

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -1,7 +1,10 @@
+import Youtube from './Youtube';
+
 function App() {
   return (
     <>
       <h1>Vite react app</h1>
+      <Youtube />
     </>
   );
 }
---
+++ 01basicvite/src/Youtube.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Youtube() {
+  return <h1>Youtube app</h1>;
+}
+
+export default Youtube;
